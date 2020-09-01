56ded3582a4a65fbaafcc8068c8d1bc311c3883e
mihailkovalenko <kovalenko_m@mirea.ru>
2020-09-02 01:06:55 +0300

    Add demo Circle

---
+++ src/ru/mirea/kovalenko/pr1/Circle/DemoCircle.java
@@ -0,0 +1,11 @@
+package ru.mirea.kovalenko.pr1.Circle;
+
+public class DemoCircle {
+    public static void main(String[] args) {
+        Circle c1 = new Circle (4.5, "red");
+        System.out.println(c1);
+        c1.setColor("green");
+        c1.setRadius(78.9);
+        System.out.println(c1);
+    }
+}
