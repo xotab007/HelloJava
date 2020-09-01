635e6fc02b8020eb23dceda58b7c59d7bed33eaf
Михаил Коваленко <mihailkovalenko@MacBook-Pro-Mihail.local>
2020-09-02 00:03:12 +0300

    Create

---
+++ src/ru/mirea/kovalenko/pr1/HelloJava.java
@@ -0,0 +1,8 @@
+package ru.mirea.kovalenko.pr1;
+import java.util.*;
+import java.lang.System;
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
