2da6341bc4da01619101ac18d8b6bc0e5c53a314
mihailkovalenko <kovalenko_m@mirea.ru>
2020-09-04 14:59:37 +0300

    AnyMatch add

---
+++ src/ru/mirea/AnyMatch.java
@@ -0,0 +1,9 @@
+package ru.mirea;
+import java.util.stream.Stream;
+
+public class AnyMatch {
+    public static void main(String[] args) {
+        Stream<Integer> infinite = Stream.iterate(2, x -> x * 2);
+        System.out.println(infinite.anyMatch(test -> test / 8  == 0));
+    }
+}
