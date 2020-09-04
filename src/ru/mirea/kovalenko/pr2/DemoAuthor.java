11d2f4f66c1e363f0e11cc7543b37657e702ce1b
mihailkovalenko <kovalenko_m@mirea.ru>
2020-09-04 15:27:21 +0300

    pr2

---
+++ src/ru/mirea/kovalenko/pr2/DemoAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.kovalenko.pr2;
+
+public class DemoAuthor {
+    public static void main(String[] args) {
+        Author n = new Author ("Mikhail","mikhail@email.ru",'m');
+        System.out.println(n);
+    }
+}
