864e08d4ac4af08196faca0daa139e056884a05f
mihailkovalenko <kovalenko_m@mirea.ru>
2020-09-18 09:38:53 +0300

    pr4

---
+++ src/ru/mirea/kovalenko/pr4/TestDisplay.java
@@ -0,0 +1,8 @@
+package ru.mirea.kovalenko.pr4;
+
+public class TestDisplay {
+    public static void main(String[] args) {
+        Display app = new Display();
+        app.setVisible(true);
+    }
+}
