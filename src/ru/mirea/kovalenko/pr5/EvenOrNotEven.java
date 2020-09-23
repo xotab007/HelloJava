85e41c9d39bd5358d5dea7ceb23a67288a807f1b
mihailkovalenko <kovalenko_m@mirea.ru>
2020-09-23 11:05:25 +0300

    pr5

---
+++ src/ru/mirea/kovalenko/pr5/EvenOrNotEven.java
@@ -0,0 +1,18 @@
+package ru.mirea.kovalenko.pr5;
+
+public class EvenOrNotEven {
+    public static int recursion(double n) {
+        if (n == 1) {
+            return 1; }
+        else if (n > 1 && n < 2) {
+            return 0; }
+        else {
+            return recursion(n / 2); }
+    }
+    public static void main(String[] args) {
+        double n = 32;
+        if (recursion(n) == 1) { System.out.println("Even");
+        } else {
+            System.out.println("Not even"); }
+    }
+}
